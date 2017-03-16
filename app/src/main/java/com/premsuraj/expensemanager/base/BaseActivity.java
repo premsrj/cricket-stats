04eb6e80537b673eed88fe4e0614cf89e8caa3f3
Premsuraj <premsuraj.s@accedo.tv>
2017-03-16 14:50:13 +0530

    cloned from expense-manager

---
+++ app/src/main/java/com/premsuraj/expensemanager/base/BaseActivity.java
@@ -0,0 +1,120 @@
+package com.premsuraj.expensemanager.base;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.premsuraj.expensemanager.R;
+import com.premsuraj.expensemanager.login.GoogleLoginManager;
+import com.premsuraj.expensemanager.navigation.NavigationContainerListener;
+import com.premsuraj.expensemanager.navigation.NavigationManager;
+
+public class BaseActivity extends AppCompatActivity implements NavigationContainerListener, GoogleLoginManager.LoginListener {
+
+    private static final String TAG = "Base";
+    GoogleLoginManager loginManager;
+    public NavigationManager navigationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action, ok?", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        loginManager = new GoogleLoginManager(this);
+        navigationManager = new NavigationManager(this);
+        navigationManager.initNavigationView((NavigationView) findViewById(R.id.nav_view));
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void closeDrawer() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public void login() {
+        loginManager.login();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        loginManager.removeAuthListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loginManager.addAuthListener();
+    }
+
+    @Override
+    public void loginSucceeded() {
+        navigationManager.userLoggedIn(loginManager.getUserDetails());
+    }
+}
---
+++ app/src/main/java/com/premsuraj/expensemanager/navigation/NavigationContainerListener.java
@@ -0,0 +1,10 @@
+package com.premsuraj.expensemanager.navigation;
+
+/**
+ * Created by Premsuraj
+ */
+
+public interface NavigationContainerListener {
+    void closeDrawer();
+    void login();
+}
